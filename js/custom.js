4c213e94cb62cfd2ae007d9dd3795e3fefa6d8ec
LatHalim <hamitovich86@mail.ru>
2020-08-30 10:28:03 +0300

    15-1

--- js/custom.js
+++ js/custom.js
@@ -4,43 +4,4 @@ $(document).ready(function(){
       });
   });
 
-  const films = [
-          {
-              start: '10.00',
-              name: 'Человек паук',
-              genre: [
-                  0,
-                  1,
-                  2
-              ]
-          },
-          {
-            start: '12.00',
-            name: 'Собачья жизнь 2',
-            genre: [
-                0,
-                1,
-                2
-            ]
-        },
-        {
-            start: '02.00',
-            name: 'Люди в черном',
-            genre: [
-                0,
-                1,
-                2
-            ]
-        },
-        {
-            start: '03.00',
-            name: 'Человек паук2',
-            genre: [
-                0,
-                1,
-                2
-            ]
-        },
-  ]
-
-  console.log(films[0])
+  
