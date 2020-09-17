37688e5c87f3a3531f18407fbcc3c42a33ec19de
LatHalim <hamitovich86@mail.ru>
2020-09-17 19:28:27 +0300

    19-1

--- js/custom.js
+++ js/custom.js
@@ -1,39 +1,39 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        nav:true,
-      });
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    nav: true,
   });
+});
 
-  const closePopupButton = document.getElementById('popup-close');
-  const openPopupButton = document.getElementById('popup-open');
-  const sendForm = document.getElementById('submit');
-  const popup = document.getElementById('popup');
+const closePopupButton = document.getElementById('popup-close');
+const openPopupButton = document.getElementById('popup-open');
+const sendForm = document.getElementById('submit');
+const popup = document.getElementById('popup');
 
-  closePopupButton.onclick = function(event) {
-      event.preventDefault();
-      popup.classList.add('hidden');
-  }
+closePopupButton.onclick = function (event) {
+  event.preventDefault();
+  popup.classList.add('hidden');
+}
 
-  openPopupButton.onclick = function(event) {
-    event.preventDefault();
-    popup.classList.remove('hidden');
+openPopupButton.onclick = function (event) {
+  event.preventDefault();
+  popup.classList.remove('hidden');
+}
+sendForm.onclick = function (event) {
+  event.preventDefault();
+  let name = document.getElementById('name');
+  let nameParent = name.parentNode;
+  let email = document.getElementById('email');
+  let checkbox = document.getElementById('checkbox');
+  nameParent.classList.remove('error');
+  nameParent.getElementsByClassName('popup-error-message')[0].innerHTML = ''
+  if (!checkInput(name.value)) {
+    nameParent.classList.add('error')
+    nameParent.getElementsByClassName('popup-error-message')[0].innerHTML = 'Заполните поле Имя'
   }
-  sendForm.onclick = function(event) {
-    event.preventDefault();
-    let name = document.getElementById('name');
-    let nameParent = name.parentNode;
-    let email = document.getElementById('email');
-    let checkbox = document.getElementById('checkbox');
-    nameParent.classList.remove('error');
-    nameParent.getElementsByClassName('popup-error-message')[0].innerHTML = ''
-    if (!checkInput(name.value)){
-        nameParent.classList.add('error')
-        nameParent.getElementsByClassName('popup-error-message')[0].innerHTML = 'Заполните поле Имя'
-    }
-  }
+}
 
-  function checkInput(value){
-      if (value)
-      return true;
-      return false;
-  }
+function checkInput(value) {
+  if (value)
+    return true;
+  return false;
+}
--- js/films.js
+++ js/films.js
@@ -1,61 +1,125 @@
-const genre_1 = 'фэнтези';
-const genre_2 = 'драма';
-const genre_3 = 'комедия';
-const genre_4 = 'мультфильм';
-const genre_5 = 'боевик';
-const genre_6 = 'приключения';
+const url = 'https://kinopoiskapiunofficial.tech/api/v2.1';
+const filmsElement = document.getElementById('films');
+const tabletFilmsElement = document.getElementsByClassName('movie-list__table')[0];
 
-const films = [{
-        start: '10.00',
-        name: 'Человек паук',
-        genre: `${genre_1},${genre_5},${genre_6}`
-    },
-    {
-        start: '12.00',
-        name: 'Собачья жизнь 2',
-        genre: `${genre_1},${genre_2},${genre_3}`
-    },
-    {
-        start: '02.00',
-        name: 'История игрушек 4',
-        genre: `${genre_4},${genre_1},${genre_3}`
-    },
-    {
-        start: '03.00',
-        name: 'Люди в черном: Интернэшнл',
-        genre: `${genre_1},${genre_5},${genre_3}`
-    },
+const films = [
+  568413,
+  530,
+  1045172,
+  1005878,
+  535341,
+  1236063
+];
 
-]
+const getFilmById = function (id) {
+  return new Promise(function (resolve, reject) {
+    fetch(`${url}/films/${id}`, {
+      headers: {
+        "X-API-KEY": "380ca7cb-582d-4bd3-b879-8b59e83d9046"
+      }
+    }).then(response => response.json()).then(resolve)
+  })
+}
 
-const film_start_1 = document.getElementById('film_start_1');
-const film_name_1 = document.getElementById('film_name_1');
-const film_genre_1 = document.getElementById('film_genre_1');
+const parseFilm = function (data) {
+  data = data.data;
+  let countries = '';
+  let genres = '';
 
-film_start_1.innerHTML = films[0].start;
-film_name_1.innerHTML = films[0].name;
-film_genre_1.innerHTML = films[0].genre;
+  data.genres.forEach(function (item) {
+    genres += `${item.genre} `
+  })
+  data.countries.forEach(function (item) {
+    countries += `${item.country} `
+  })
+  return {
+    name: data.nameRu,
+    country: countries,
+    genre: genres,
+    year: data.year,
+    description: data.description,
+    img: data.posterUrl,
+    link: data.webUrl
+  }
+}
+const generateFilmItem = function ({
+  name,
+  country,
+  genre,
+  year,
+  description,
+  img,
+  link
+}) {
+  return ` 
+        <div class="cinema__movie1">
+                    <div class="cinema__relative">
+                        <div class="cinema__img">
+                            <img src="${img}" alt="">
+                        </div>
+                        <div class="cinema__descr">
+                            <div class="cinema__text1">${name}</div>
+                            <div class="cinema__sep"></div>
+                            <div class="cinema__text2">${description}</div>
+                            <div class="cinema__linis">
+                                <a href="#"><i class="cinema__icon icon_facebook"><img src="img/facebook  .png"
+                                            alt="фейсбук"></i></a>
+                                <a href="#"><i class="cinema__icon icon_twiter"><img src="img/twitter.png"
+                                            alt="твитер"></i></a>
+                                <a href="#"><i class="cinema__icon icon_behance"><img src="img/behance.png"
+                                            alt="беханс"></i></a>
+                            
+                            </div>
+                        </div>
+                </div>
+          </div>
+          `;
+}
 
-const film_start_2 = document.getElementById('film_start_2');
-const film_name_2 = document.getElementById('film_name_2');
-const film_genre_2 = document.getElementById('film_genre_2');
+const generatTabletItem = function ({
+  name,
+  genre
+}) {
+  function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
-film_start_2.innerHTML = films[1].start;
-film_name_2.innerHTML = films[1].name;
-film_genre_2.innerHTML = films[1].genre;
+  let time1, time2, time3, time4;
+  time1 = getRandomNumber(0, 2);
+  time3 = getRandomNumber(0, 5);
+  time4 = getRandomNumber(0, 9);
+  switch (time1) {
+    case 0:
+    case 1:
+      time2 = getRandomNumber(0, 9);
+    default:
+      time2 = getRandomNumber(0, 3);
+  };
 
-const film_start_3 = document.getElementById('film_start_3');
-const film_name_3 = document.getElementById('film_name_3');
-const film_genre_3 = document.getElementById('film_genre_3');
+  const time = `${time1}${time1}:${time3}${time4}`;
+  return `
+  <tr>
+      <td id="film_start_2">${time}</td>
+      <td id="film_name_2">${name}</td>
+      <td id="film_genre_2">${genre}</td>
+      <td><img src="img/plus.png " alt="plus" class="movie-list__table_plus"></td>
+  </tr>
+`;
+};
 
-film_start_3.innerHTML = films[2].start;
-film_name_3.innerHTML = films[2].name;
-film_genre_3.innerHTML = films[2].genre;
-
-const film_start_4 = document.getElementById('film_start_4');
-const film_name_4 = document.getElementById('film_name_4');
-const film_genre_4 = document.getElementById('film_genre_4');
-
-film_start_4.innerHTML = films[3].start;
-film_name_4.innerHTML = films[3].name;
-film_genre_4.innerHTML = films[3].genre;
+let element, prepareFilm;
+films.forEach(function (item) {
+  let film = getFilmById(item);
+  film.then(result => {
+    prepareFilm = parseFilm(result);
+    element = generateFilmItem({
+      ...prepareFilm
+    });
+    tabletElement = generatTabletItem({
+      ...prepareFilm
+    });
+    filmsElement.insertAdjacentHTML('beforeEnd', element);
+  })
+})
